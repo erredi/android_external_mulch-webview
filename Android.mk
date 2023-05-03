750fe0f2a56392eb2bec68810e3e34e0910430e6
erredi <64195203+erredi@users.noreply.github.com>
2023-05-03 18:51:50 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -38,7 +38,7 @@ LOCAL_REQUIRED_MODULES := \
 
 LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/SystemWebView.apk
+LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/MulchSystemWebView.apk
 
 LOCAL_PREBUILT_JNI_LIBS_arm := @lib/armeabi-v7a/libwebviewchromium.so
 LOCAL_PREBUILT_JNI_LIBS_arm64 := @lib/arm64-v8a/libwebviewchromium.so
