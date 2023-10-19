54a19a70322abf9f9fd99c454ee8710b3f34a8d6
erredi <riccardodurante@tuta.io>
2023-10-19 17:37:25 +0200

    Arm64 build only

--- Android.mk
+++ Android.mk
@@ -28,9 +28,7 @@ LOCAL_REQUIRED_MODULES := \
         libwebviewchromium_loader \
         libwebviewchromium_plat_support
 
-LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
-my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/webview.apk
+LOCAL_SRC_FILES := prebuilt/arm64/webview.apk
 
 # Don't sign our arm* builds for out-of-band distribution
 ifneq ( ,$(filter $(my_src_arch), arm arm64))
