56abfc14a3b5558137889a21dc62ea65e2f03ad8
erredi <riccardodurante@tuta.io>
2023-10-25 18:11:06 +0200

    Override standard android webview

--- Android.mk
+++ Android.mk
@@ -19,8 +19,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_OVERRIDES_PACKAGES := webview
 
-LOCAL_MODULE := webview
+LOCAL_MODULE := mulch-webview
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRODUCT_MODULE := true
 LOCAL_MULTILIB := both
@@ -28,7 +29,9 @@ LOCAL_REQUIRED_MODULES := \
         libwebviewchromium_loader \
         libwebviewchromium_plat_support
 
-LOCAL_SRC_FILES := prebuilt/arm64/webview.apk
+LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
+my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
+LOCAL_SRC_FILES := prebuild/$(my_src_arch)/webview.apk
 
 # Don't sign our arm* builds for out-of-band distribution
 ifneq ( ,$(filter $(my_src_arch), arm arm64))
@@ -43,5 +46,3 @@ LOCAL_PREBUILT_JNI_LIBS_x86 := @lib/x86/libwebviewchromium.so
 LOCAL_PREBUILT_JNI_LIBS_x86_64 := @lib/x86_64/libwebviewchromium.so
 
 include $(BUILD_PREBUILT)
-
-
